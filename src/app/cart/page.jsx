3fd6fdc23dbd9e12ffd7e58c8aee00a3249e8c2d
Bhoami K Khona <bhoamikhona@gmail.com>
2025-11-09 16:29:26 -0500

    Cart Skeleton

---
+++ src/app/cart/page.jsx
@@ -0,0 +1,10 @@
+import Container from "@/components/layout/Container.jsx";
+import CartList from "@/components/cart/CartList.jsx";
+import CartSummary from "@/components/cart/CartSummary.jsx";
+
+export default function Cart() {
+  <Container>
+    <CartList />
+    <CartSummary />
+  </Container>;
+}
