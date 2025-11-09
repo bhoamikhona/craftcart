951a2380ed936cb0a7941836ee8ac4303eed0690
Bhoami K Khona <bhoamikhona@gmail.com>
2025-11-09 15:18:14 -0500

    update users dummy data

--- src/data/users.js
+++ src/data/users.js
@@ -6,7 +6,7 @@ export const users = [
     phone: "+1 (212) 555-2145",
     address: "245 E 14th St, New York, NY 10003",
     creatorId: 1,
-    tutorials: [1, 4],
+    tutorials: [1, 4], // Sarah created tutorials 1 and 4
     followers: 1247,
     subscribedChannels: [2, 3],
   },
@@ -17,7 +17,7 @@ export const users = [
     phone: "+1 (646) 555-7712",
     address: "88 Washington Pl, New York, NY 10011",
     creatorId: 2,
-    tutorials: [2, 5],
+    tutorials: [2, 5], // Mike created tutorials 2 and 5
     followers: 986,
     subscribedChannels: [1, 3, 4],
   },
@@ -28,7 +28,7 @@ export const users = [
     phone: "+1 (917) 555-3342",
     address: "310 Amsterdam Ave, New York, NY 10024",
     creatorId: 3,
-    tutorials: [3],
+    tutorials: [3], // Emma created tutorial 3
     followers: 752,
     subscribedChannels: [1, 2, 5],
   },
@@ -39,7 +39,7 @@ export const users = [
     phone: "+1 (347) 555-9908",
     address: "57 W 16th St, New York, NY 10011",
     creatorId: 4,
-    tutorials: [],
+    tutorials: [], // Lisa hasn’t uploaded yet
     followers: 523,
     subscribedChannels: [1, 2, 3],
   },
@@ -50,7 +50,7 @@ export const users = [
     phone: "+1 (929) 555-6041",
     address: "401 Hudson St, New York, NY 10014",
     creatorId: 5,
-    tutorials: [],
+    tutorials: [], // Viewer for now, can upload later
     followers: 45,
     subscribedChannels: [1, 2, 3, 4],
   },
