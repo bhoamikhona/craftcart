3e3fd8abd3458b0c34562832e0dfde2eb4fd2724
Negar Kamvij <negarkamvizh@gmail.com>
2025-11-16 14:23:41 -0500

    Add Supabase client setup and update NextAuth to use Supabase

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,36 +1,55 @@
+import supabase from "@/lib/supabaseClient";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth ({
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email:{labale: "Email", type: "email"},
-                password: {lable: "Password", type: "password"},
-            },
-            async authorize(credentials){
-                const {email, password} = credentials;
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
 
-                //must be replaced with our real database:
-                if(email === "test@example.com" && password === "123456"){
-                    return{ id: 1, name: "Test User", email: "test@example.com"};
-                }
-                // if there is nothing matched the login will fail
-                return null;
-            },
-        }),
-    ],
+      async authorize(credentials) {
+        const { email, password } = credentials;
 
-    pages: {
-        signIn: "/login",
-    },
+        // 1. Look for the user in Supabase
+        const { data: user, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("email", email)
+          .single();
 
-    sessions: {
-        strategy: "jwt",
-    },
+        // If email not found, login fails
+        if (!user) {
+          return null;
+        }
 
-    secret: process.env.NEXTAUTH_SECRET,
+        // 2. Compare passwords (plain text for midterm project)
+        if (user.password !== password) {
+          return null;
+        }
+
+        // 3. Login success,  return this user info
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+
+  pages: {
+    signIn: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
