69ad5c601e6c8e860bae4950b88633a040562c37
Bhoami K Khona <bhoamikhona@gmail.com>
2025-11-17 05:48:03 -0500

    Testimonials Section

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Home/Hero/Hero.jsx";
 import Features from "@/components/Home/Features/Features.jsx";
+import Testimonials from "@/components/Home/Testimonials/Testimonials.jsx";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Features />
+      <Testimonials />
     </div>
   );
 }
---
+++ src/components/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,28 @@
+import "./Testimonials.css";
+
+export default function Testimonials() {
+  return (
+    <section className="section__testimonials">
+      <div className="grid grid__3-cols">
+        <div className="testimonials__img-container">
+          <img
+            className="testimonials__img"
+            src="./images/testimonials/testimonial.jpg"
+            alt="two girls crafting together"
+          />
+        </div>
+        <div className="testimonials__container">
+          <h2 className="testimonials__title">
+            {`\"`}We never thought crafting could be this easy and fun!{`\"`}
+          </h2>
+          <blockquote className="testimonials__text">
+            CraftCart’s tutorials helped us learn new skills we didn’t even know
+            we needed. From candle making to simple home décor, we’ve created
+            things we’re actually proud of! Such a warm and inspiring platform.
+          </blockquote>
+          <p class="testimonial-author">— Mary and Sarah Johnson</p>
+        </div>
+      </div>
+    </section>
+  );
+}
