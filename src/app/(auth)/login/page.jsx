1989169d01f4e255f4bb897d397a60badf77f06a
Negar Kamvij <negarkamvizh@gmail.com>
2025-11-17 14:53:58 -0500

    Redirect user to /profile after successful login

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
     if (result.error) {
       setError("Invalid email or password.");
     } else {
-      router.push("/");
+      router.push("/profile");
     }
   }
 
