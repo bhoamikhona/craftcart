196bab918b33dc8c0db42992aff23ef868a16c95
Janvi Patel <janvipatel3052@gmail.com>
2025-12-15 23:48:55 -0500

    Fixed key issues

--- src/components/cart/CartList.jsx
+++ src/components/cart/CartList.jsx
@@ -50,11 +50,10 @@ export default function CartList({ cart, updateQuantity }) {
   return (
     <div className="max-w-4xl mx-auto bg-white rounded-xl shadow p-6 flex flex-col gap-6">
 
-      {cart.map(item => (
+      {cart.map((item) => (
         <CartItem
-          key={item.id}            // ← key goes here!
+          key={item.id}   // or item.product_id / order_item_id
           item={item}
-          updateQuantity={updateQuantity}
         />
       ))}
 
