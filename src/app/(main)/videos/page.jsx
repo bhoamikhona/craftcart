228401fe651abfd5300138a30bb24db7f3d1afd1
Janvi Patel <janvipatel3052@gmail.com>
2025-12-15 23:49:20 -0500

    Modified UI to make it responsive

--- src/app/(main)/videos/page.jsx
+++ src/app/(main)/videos/page.jsx
@@ -1,9 +1,21 @@
 import VideoCard from "@/components/ui/VideoCard.jsx";
-import {data} from "@/data/videos.js";
+import { data } from "@/data/videos.js";
 
 export default function Videos() {
   return (
-    <div className="mx-auto max-w-7xl grid grid-cols-3 gap-12 mt-12 mb-20">
+    <div
+      className="
+        max-w-7xl
+        mx-auto
+        px-4 sm:px-6 lg:px-8
+        mt-12 mb-20
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        lg:grid-cols-3
+        gap-6 lg:gap-12
+      "
+    >
       {data.map((v) => (
         <VideoCard key={v.id} v={v} />
       ))}
